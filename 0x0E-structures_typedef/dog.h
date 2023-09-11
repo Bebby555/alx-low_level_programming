15f5fbd426592a328057032812bdde82f034ca3a
debby555 <deborahorakwue1@gmail.com>
2023-09-11 13:08:41 -0700

    Doggy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,23 @@
+#ifndef dogstruct
+#define dogstruct
+/**
+ * struct dog -short description
+ * @name: 1st member
+ * @age: 2nd member
+ * @owner: 3rd member
+ *
+ * Description: Longer description
+ */
+struct dog
+};
+/**
+ * dog_t -new type of struct dog
+ */
+typedef struct dog dog_t;
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+int _strlen(char *s);
+char *_strcpy(char *dest, char *src);
+#endif
