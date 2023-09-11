ef067918eba0070565f73b9cddae11fb9d718b7e
debby555 <deborahorakwue1@gmail.com>
2023-09-11 13:30:48 -0700

    Whatsupp

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,23 +1,27 @@
-#ifndef dogstruct
-#define dogstruct
+#ifndef DOG_H
+#define DOG_H
+
 /**
  * struct dog -short description
- * @name: 1st member
- * @age: 2nd member
- * @owner: 3rd member
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: name of owner
  *
  * Description: Longer description
  */
+
 struct dog
+{
+	char *name;
+	float age;
+	char *owner;
 };
-/**
- * dog_t -new type of struct dog
- */
-typedef struct dog dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 int _strlen(char *s);
 char *_strcpy(char *dest, char *src);
+
 #endif
